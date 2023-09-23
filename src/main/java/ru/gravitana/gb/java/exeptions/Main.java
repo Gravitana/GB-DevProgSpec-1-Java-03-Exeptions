446c3eb33237d103c0b3aa8a85fcc3dd8fc54bba
Виталий Грахов <v.a.grahov@yandex.ru>
2023-09-23 21:08:30 +0500

    init

---
+++ src/main/java/ru/gravitana/gb/java/exeptions/Main.java
@@ -0,0 +1,7 @@
+package ru.gravitana.gb.java.exeptions;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("GB-DevProgSpec-1-Java-03-Exeptions");
+    }
+}
