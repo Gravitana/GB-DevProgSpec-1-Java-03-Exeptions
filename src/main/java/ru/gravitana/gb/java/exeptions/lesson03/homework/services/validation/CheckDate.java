057c77db4eda4c3de2bd97522a72b6db8e4a388e
Виталий Грахов <v.a.grahov@yandex.ru>
2023-10-03 00:30:49 +0500

    fix: check date range

--- src/main/java/ru/gravitana/gb/java/exeptions/lesson03/homework/services/validation/CheckDate.java
+++ src/main/java/ru/gravitana/gb/java/exeptions/lesson03/homework/services/validation/CheckDate.java
@@ -5,14 +5,26 @@ import ru.gravitana.gb.java.exeptions.lesson03.homework.exceptions.InputDataExce
 import java.time.DateTimeException;
 import java.time.LocalDate;
 
+import static java.time.LocalDate.now;
+
 public class CheckDate {
     public static boolean check(String str) throws InputDataException {
-        String[] date = str.split("\\.");
+        String[] dateStr = str.split("\\.");
+        LocalDate date;
         try {
-            int year = Integer.parseInt(date[2]);
-            int month = Integer.parseInt(date[1]);
-            int day = Integer.parseInt(date[0]);
-            LocalDate.of(year, month, day);
+            int year = Integer.parseInt(dateStr[2]);
+            int month = Integer.parseInt(dateStr[1]);
+            int day = Integer.parseInt(dateStr[0]);
+            date = LocalDate.of(year, month, day);
+            LocalDate now = now();
+            int difference = now.getYear() - date.getYear();
+
+            if (difference < 7) {
+                throw new InputDataException("Возраст должен быть более 7 лет");
+            }
+            if (difference > 110) {
+                throw new InputDataException("Возраст должен быть менее 110 лет");
+            }
         } catch (DateTimeException e) {
             throw new InputDataException("Неправильный формат даты");
         }
